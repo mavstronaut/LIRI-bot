4f90b79e85fe9ae186740dc4ab8ea96fa6304a5e
mavstronaut <mavbarona@gmail.com>
2019-07-21 14:22:40 -0400

    adding liri.js file

---
+++ liri.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+
+/* COMMANDS
+
+   * `concert-this`
+
+   * `spotify-this-song`
+
+   * `movie-this`
+
+   * `do-what-it-says`
+*/
+
