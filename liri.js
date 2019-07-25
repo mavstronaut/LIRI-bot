7a30830be846c11b334687853cc8ebaf2c28c623
mavstronaut <mavbarona@gmail.com>
2019-07-25 19:47:02 -0400

    updating

--- liri.js
+++ liri.js
@@ -150,5 +150,4 @@ function doWhatItSays() {
     //  * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
     //  * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
     //  * Edit the text in random.txt to test out the feature for movie-this and concert-this.
-
 };
